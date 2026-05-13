2eb379776048b0574357bd4bd44ef3f0b648a12b
Pache360 <rubielow@gmail.com>
2026-05-13 11:43:20 -0600

    RacSys v3.35

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co', // Autoriza las imágenes que vienen de tu Supabase
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
